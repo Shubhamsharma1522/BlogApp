743fbcf88356c4ccc1723458272999b312129078
Shubhamsharma1522 <shubhamsharma.in1522@gmail.com>
2023-12-25 16:48:55 +0530

    name changing

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo() {
   return (
-  <img className='h-20' src="/BlogApp Logo.png" alt="blog logo" />
+  <img className='h-20' src="/BlogApp Logo.png" alt="Blog Logo" />
   )
 }
 
